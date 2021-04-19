4faabebf7c02eae961b67a742203549b445a211a
Claire <claire.cy.t@gmail.com>
2021-04-18 21:12:51 -0400

    Add card-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.components';
 
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      monsters:[],
+      monsters: [],
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
-    .then(users => this.setState({monsters: users}));
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (<h1 key={monster.id}>{monster.name}</h1>))}
+        <CardList>{this.state.monsters.map(monster => (<h1 key={monster.id}>{monster.name}</h1>))}</CardList>
       </div>
     );
   }
